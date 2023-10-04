9ede8c488268952aea64f4a327e946f72c889508
JisNOTmyname <145459296+JisNOTmyname@users.noreply.github.com>
2023-10-04 20:37:53 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const logobar = document.getElementById('logobar');
+logobar.style.color = 'red';
+
+var newLi = document.createElement('li');
+
+newLi
+
+var newA = document.createElement('a');
+
+
